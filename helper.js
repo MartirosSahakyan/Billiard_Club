c4b43038297165d0c928515dbc754da2b6e9aeba
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-06-15 18:44:19 +0400

    feat(helper.js): divide functional from index.js

--- helper.js
+++ helper.js
@@ -1,2 +1,11 @@
-
-// export default setCurrentDate
+function setCurrentDate(tag) {
+  const makeTwoDigit = (num) => (String(num).length === 1 ? `0${num}` : num);
+  const dateNow = new Date();
+  let year = dateNow.getFullYear();
+  let month = makeTwoDigit(dateNow.getMonth() + 1);
+  let date = dateNow.getDate();
+  let hour = dateNow.getHours();
+  let minute = dateNow.getMinutes();
+  let dateValue = `${year}-${month}-${date}T${hour}:${minute}`;
+  tag.setAttribute("value", dateValue);
+}
--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-// import {setCurrentDate} from "./helper";
-function setCurrentDate(tag) {
-  const makeTwoDigit = (num) => (String(num).length === 1 ? `0${num}` : num);
-  const dateNow = new Date();
-  let year = dateNow.getFullYear();
-  let month = makeTwoDigit(dateNow.getMonth() + 1);
-  let date = dateNow.getDate();
-  let hour = dateNow.getHours();
-  let minute = dateNow.getMinutes();
-  let dateValue = `${year}-${month}-${date}T${hour}:${minute}`;
-  tag.setAttribute("value", dateValue);
-}
-
 const barIcon = document.querySelector(".bar-icon");
 const mobileMenu = document.querySelector(".mobile-menu__container");
 const userDate = document.querySelector("#date");
