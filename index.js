063d164997e36c3ee312731e883227a7415fcd92
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-06-15 17:58:56 +0400

    feat(index.js): add function setting current date in order_form date field

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+// import {setCurrentDate} from "./helper";
+function setCurrentDate(tag) {
+  const makeTwoDigit = (num) => (String(num).length === 1 ? `0${num}` : num);
+  const dateNow = new Date();
+  let year = dateNow.getFullYear();
+  let month = makeTwoDigit(dateNow.getMonth() + 1);
+  let date = dateNow.getDate();
+  let hour = dateNow.getHours();
+  let minute = dateNow.getMinutes();
+  let dateValue = `${year}-${month}-${date}T${hour}:${minute}`;
+  tag.setAttribute("value", dateValue);
+}
+
 const barIcon = document.querySelector(".bar-icon");
 const mobileMenu = document.querySelector(".mobile-menu__container");
-barIcon.addEventListener("click", function() {
+const userDate = document.querySelector("#date");
+setCurrentDate(userDate);
+
+barIcon.addEventListener("click", function () {
   if (mobileMenu.style.display === "block") {
     mobileMenu.style.display = "none";
   } else {
     mobileMenu.style.display = "block";
   }
 });
-
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
