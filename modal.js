7c5d3b2bcc6b9850b2da4d521d40043280dababc
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-06-16 01:55:13 +0400

    feat(modal.css): add styles for modal, feat(modal.js): add logic for modal, feat(index.html): add modal DOM structure

---
+++ modal.js
@@ -0,0 +1,27 @@
+let modal = document.querySelector(".modal");
+let openModalBtn = document.querySelectorAll(".card-btn");
+let closeModalBtn = document.querySelector(".modal__close");
+let modalTable = document.querySelector("#modal-table");
+let modalPlace = document.querySelector("#modal-place");
+
+openModalBtn.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    modal.style.display = "block";
+    let dataTable = btn.getAttribute("data-table");
+    let dataPlace = btn.getAttribute("data-place");
+    // modalTable.setAttribute('value',dataTable)
+    // modalPlace.setAttribute('value',dataPlace)
+    modalTable.textContent = dataTable.toUpperCase();
+    modalPlace.textContent = dataPlace.toUpperCase();
+  });
+});
+
+closeModalBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
