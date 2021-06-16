4bbccbb3044f089ee4a8484448096e4d08c4c86b
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-06-17 00:02:29 +0400

    fix(index.js): optimize bar icon rotate function

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,17 @@ const mobileMenu = document.querySelector(".mobile-menu__container");
 const userDate = document.querySelector("#date");
 const slides = document.querySelectorAll(".slide");
 
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+
 setCurrentDate(userDate);
 
-barIcon.addEventListener("click", function () {
-  barIcon.classList.toggle("bar-icon__change");
+barIcon.addEventListener("click", () => {
+  bar1.classList.toggle("bar-rotate-minus");
+  bar2.classList.toggle("bar-delete");
+  bar3.classList.toggle("bar-rotate-plus");
+
   if (mobileMenu.style.display === "block") {
     mobileMenu.style.display = "none";
   } else {
