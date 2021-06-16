36ce680a5470642ad2c6eff270b444556aca246d
Martiros Sahakyan <martiros.sahakyan.1992@gmail.com>
2021-06-16 18:07:01 +0400

    feat: add slider dizayn ang logic

---
+++ scripts/slide.js
@@ -0,0 +1,27 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  const slides = document.querySelectorAll(".slide");
+  const dots = document.querySelectorAll(".slide__dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("slide__dot-active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " slide__dot-active";
+}
